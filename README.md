d69a094573ada854008d7a9320ee8e313a9ae3d4
Kevin Chacon <kevinchacon3211@gmail.com>
2023-03-02 20:26:07 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Task List w/ React
+A simple task list made with React and Tailwind.
---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+function App() {
+  
+  const [tasks, setTasks] = useState([])
+  const [newTask, setNewTask] = useState('')
+
+  const addTask = () => {
+      setNewTask('')
+      if (newTask) {
+        let newTaskId = tasks.length + 1
+        setTasks([
+          ...tasks,
+          {id: newTaskId, text: newTask}
+        ])
+      }
+  }
+
+  return (
+    <div>
+      <nav className='flex items-center bg-cyan-500 w-screen h-16'>
+        <h1 className='font-bold text-lg text-white ml-4'>Task List w/React</h1>
+      </nav>
+      <main className='flex justify-center m-3 text-2xl'>
+        <div className='w-full md:w-1/2'>
+          <h2 className='font-semibold text-center p-3'>Your Tasks</h2>
+          <div className='flex place-content-between mb-3'>
+            <input type="text" className='w-10/12 p-2 border border-gray-300' value={newTask} onChange={e => setNewTask(e.target.value)} placeholder='Write a new task here' />
+            <button className='bg-cyan-300 text-white p-2' onClick={addTask}>Add Task</button>
+          </div>
+          <div className='w-full h-fit bg-slate-200'>
+            <ul className='w-full h-full'>
+              {
+                tasks.map(task => (<li key={task.id} className='p-3 border-2 border-white'>{task.text}</li>))
+              }
+            </ul>
+          </div>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
